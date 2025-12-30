2844a02ae63ec4310997470833f84d4b50a0fe4f
diyakd2499 <diyakd2499@gmail.com>
2025-12-30 10:18:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
-app.use("/api/auth", require("./routes/auth.routes"));
-app.use("/api/subjects", require("./routes/subject.routes"));
-app.use("/api/lectures", require("./routes/lecture.routes"));
-app.use("/api/notifications", require("./routes/notification.routes"))
-app.use("/api/attendance", require("./routes/attendance.routes"));
-app.use("/api/references", require("./routes/reference.routes"));
+app.use("/api/auth", require("../routes/auth.routes"));
+app.use("/api/subjects", require("../routes/subject.routes"));
+app.use("/api/lectures", require("../routes/lecture.routes"));
+app.use("/api/notifications", require("../routes/notification.routes"))
+app.use("/api/attendance", require("../routes/attendance.routes"));
+app.use("/api/references", require("../routes/reference.routes"));
 app.use("/uploads", express.static("uploads"));
 
 mongoose.connect(process.env.MONGO_URI)
