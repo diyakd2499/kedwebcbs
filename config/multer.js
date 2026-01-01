26c7d0d63f8eca4405fbe61edd7d0a01380ae695
diyakd2499 <diyakd2499@gmail.com>
2026-01-01 14:31:55 +0200

    Update multer.js

--- config/multer.js
+++ config/multer.js
@@ -2,20 +2,17 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-destination: (req, file, cb) => {
-
-  cb(null, "uploads/lectures");
-  }}
-  const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads");
+    // تم تغيير المسار ليكون المجلد الرئيسي مباشرة ليتوافق مع رابط الواجهة
+    cb(null, "uploads"); 
   },
   filename: (req, file, cb) => {
-    const uniqueName =
-      Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null,uniqueName + path.extname(file.originalname));
+    const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    // تأكد من كتابة null بحرفي l فقط
+    cb(null, uniqueName + path.extname(file.originalname));
   }
 });
+
 
 const upload = multer({ storage });
 
