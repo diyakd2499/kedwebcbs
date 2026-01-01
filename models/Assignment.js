d148bb80e7f954f0d5c492c31b7c24825eb9fc39
diyakd2499 <diyakd2499@gmail.com>
2026-01-01 13:28:47 +0200

    Add files via upload

---
+++ models/Assignment.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const AssignmentSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "العنوان مطلوب"],
+      trim: true
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    type: {
+      type: String,
+      enum: ["exercise", "research", "project"],
+      required: [true, "نوع التكليف مطلوب"]
+    },
+    subject: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Subject",
+      required: [true, "المادة الدراسية مطلوبة"]
+    },
+    file: {
+      type: String,
+      required: [true, "يجب رفع ملف التكليف"]
+    },
+    dueDate: {
+      type: Date
+    },
+    addedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Assignment", AssignmentSchema);
